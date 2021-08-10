581a862307e32f532152462fd64a812f428fb89a
winston <whinstondj@gmail.com>
2021-08-09 20:55:34 -0500

    Cerrar Alertas y navegar
    Creamos el codigo para leer una alerta y Maximizar la ventana

--- src/com/sapiensschool/automatizacion/ManejadorNavegador.java
+++ src/com/sapiensschool/automatizacion/ManejadorNavegador.java
@@ -2,9 +2,12 @@ package com.sapiensschool.automatizacion;
 
 import java.util.ArrayList;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class ManejadorNavegador {
 
@@ -14,9 +17,15 @@ public class ManejadorNavegador {
 		//Here we will create and launch our browser
 		WebDriver customDriver = new ChromeDriver();		
 		customDriver.get("https://www.google.com/");
+		customDriver.manage().window().maximize();
 		customDriver.navigate().to("https://winstoncastillo.com/robot-selenium");
 		System.out.println(customDriver.getCurrentUrl());
 		System.out.println(customDriver.getTitle());
+		//Codigo para crear acciones del navegador
+		Actions mouseAction = new Actions(customDriver);
+		WebElement menuOption = customDriver.findElement(By.xpath("//*[@id=\"menu\"]/div[2]/ul/li[2]/a"));
+		mouseAction.moveToElement(menuOption).perform();
+		Thread.sleep(3000);		
 		//Este comando presiona la flecha de regreso
 		customDriver.navigate().back();
 		//Este comando presiona la flecha hacia adelante
@@ -39,7 +48,13 @@ public class ManejadorNavegador {
 		codeToOpenTab.executeScript("window.open();");
 		tabNames = new ArrayList<String>(customDriver.getWindowHandles());
 		customDriver.switchTo().window(tabNames.get(2));
-		customDriver.get("https://www.amazon.com/");
+		customDriver.get("http://winstoncastillo.com/ejercicios/atoc.html");
+		customDriver.findElement(By.cssSelector("#testCasesAtoC > p:nth-child(2) > button")).click();
+		Thread.sleep(3000);
+		String message = customDriver.switchTo().alert().getText();
+		System.out.println("Este es el mensaje de la ventana: " + message);
+		customDriver.switchTo().alert().accept();
+		Thread.sleep(3000);
 		customDriver.close();
 		customDriver.switchTo().window(tabNames.get(1));
 		customDriver.close();
