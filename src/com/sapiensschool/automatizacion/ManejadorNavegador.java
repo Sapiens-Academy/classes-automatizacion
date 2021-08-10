469371b99652b3ba3a751f27f8e8e3670685910e
winston <whinstondj@gmail.com>
2021-08-09 20:10:32 -0500

    Arrays y cierre de pestanas
    - Añadimos lineas de código para abrir nuevas pestañas y luego cerrarlas

--- src/com/sapiensschool/automatizacion/ManejadorNavegador.java
+++ src/com/sapiensschool/automatizacion/ManejadorNavegador.java
@@ -1,12 +1,14 @@
 package com.sapiensschool.automatizacion;
 
+import java.util.ArrayList;
+
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ManejadorNavegador {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// Instanciar y lanzar el navegador
 		System.setProperty("webdriver.chrome.driver", "D:\\driversParaNavegadores\\chromedriver.exe");
 		//Here we will create and launch our browser
@@ -23,15 +25,28 @@ public class ManejadorNavegador {
 		customDriver.navigate().refresh();
 		//Vamos a abrir una segunda pestana
 		String firstTab = customDriver.getWindowHandle();
-		System.out.println(firstTab);
+		//System.out.println(firstTab);
 		Integer sizeOfTabs = customDriver.getWindowHandles().size();
-		System.out.println(sizeOfTabs);
+		//System.out.println(sizeOfTabs);
 		JavascriptExecutor codeToOpenTab = (JavascriptExecutor)customDriver;
 		codeToOpenTab.executeScript("window.open();");
 		sizeOfTabs = customDriver.getWindowHandles().size();
-		System.out.println(sizeOfTabs);
+		//System.out.println(sizeOfTabs);
 		//Este comando cierra la pestana
-		//customDriver.close();
+		ArrayList<String> tabNames = new ArrayList<String>(customDriver.getWindowHandles());
+		customDriver.switchTo().window(tabNames.get(1));
+		customDriver.get("https://www.google.com/");
+		codeToOpenTab.executeScript("window.open();");
+		tabNames = new ArrayList<String>(customDriver.getWindowHandles());
+		customDriver.switchTo().window(tabNames.get(2));
+		customDriver.get("https://www.amazon.com/");
+		customDriver.close();
+		customDriver.switchTo().window(tabNames.get(1));
+		customDriver.close();
+		customDriver.switchTo().window(tabNames.get(0));
+		Thread.sleep(1000);
+		customDriver.close();
+		//customDriver.quit();
 	}
 
 }
